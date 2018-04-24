ddc5624a4197c83203fc2cac56334d298b1ed63b
Carlos Alberto Diaz Arze <cdiaz1@miuandes.cl>
2018-04-24 19:12:56 -0300

    Node initialization

---
+++ Binary-Tree/Binary-Tree/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    class Node<T>
+    {
+        public T information { get; set; }
+        //public Node<T> parentNode {get;}
+        public Node<T> rChildNode {get; set;}
+        public Node<T> lChildNode {get; set;}
+
+        public Node(T information)
+        {
+            this.information = information;
+            rChildNode = null;
+            lChildNode = null;
+        }
+
+
+    }
+
+}
